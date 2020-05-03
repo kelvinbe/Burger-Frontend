7030c60de0c92293f87ba76d2ffb019c3d1ecdc9
kelvin beno <kelvinbeno526@gmail.com>
2020-05-03 09:36:12 +0300

    laout set-up

--- burger-front-end/src/App.js
+++ burger-front-end/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Layout from '../src/components/Layout/Layout'
 
 class App extends Component{
   render() {
@@ -6,6 +7,9 @@ class App extends Component{
   
   return (
     <div>
+      <Layout>
+        <p>Testing</p>
+      </Layout>
       
     </div>
   );
