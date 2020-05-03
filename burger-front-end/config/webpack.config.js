7a9c0da9fc8479e81b414ae34bc03b9499caa16f
kelvin beno <kelvinbeno526@gmail.com>
2020-05-03 09:13:29 +0300

    set up done

--- burger-front-end/config/webpack.config.js
+++ burger-front-end/config/webpack.config.js
@@ -81,7 +81,8 @@ module.exports = function(webpackEnv) {
       },
       {
         loader: require.resolve('css-loader'),
-        options: cssOptions,
+        options: cssOptions
+
       },
       {
         // Options for PostCSS as we reference these options twice
