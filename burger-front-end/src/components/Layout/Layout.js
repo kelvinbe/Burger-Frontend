f9d05799940911baba112ef919cad4d1f82d3d95
kelvin beno <kelvinbeno526@gmail.com>
2020-05-04 05:50:24 +0300

    added proptypes to burger ingredient

--- burger-front-end/src/components/Layout/Layout.js
+++ burger-front-end/src/components/Layout/Layout.js
@@ -4,12 +4,10 @@ import './Layout.css'
 
 const layout = (props) => (
     <Aux>
-
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className='Content'>
             {props.children}
         </main>
-        
     </Aux>
 );
 export default layout
