0b522a73ab00c5fad99bdc805231e88ec5a7e0e9
kelvin beno <kelvinbeno526@gmail.com>
2020-05-21 18:07:46 +0300

    added the orders page to the application

--- burger-front-end/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger-front-end/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,8 +6,8 @@ const navigationItems = (props) => (
     // eslint-disable-next-line no-unused-expressions
     <div className={"NavigationItems"}>
     <ul className={"ul"}>
-        <NavigationItem link="/" active>Burger Builder</NavigationItem>
-        <NavigationItem link="/">Checkout</NavigationItem>
+        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+        <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
     </div>
  
