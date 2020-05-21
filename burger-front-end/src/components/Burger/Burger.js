8f0fa51e400b72f26ed00e92c5acaf866c45e89b
kelvin beno <kelvinbeno526@gmail.com>
2020-05-21 13:17:11 +0300

    added the contact summary component and functionality

--- burger-front-end/src/components/Burger/Burger.js
+++ burger-front-end/src/components/Burger/Burger.js
@@ -1,32 +1,33 @@
-import React from 'react'
-import './Burger.css'
-import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
-
+import React from "react";
+import { withRouter } from "react-router-dom";
+import "./Burger.css";
+import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 
 const burger = (props) => {
-    let transformedIngredients = Object.keys(props.ingredients).map(igkey => {
-        return [...Array(props.ingredients[igkey])].map((_, i) => {
-            // eslint-disable-next-line no-unused-expressions
-            return <BurgerIngredient key={igkey + i} type={igkey} />
-        })
+    console.log(props)
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igkey) => {
+      return [...Array(props.ingredients[igkey])].map((_, i) => {
+        // eslint-disable-next-line no-unused-expressions
+        return <BurgerIngredient key={igkey + i} type={igkey} />;
+      });
     })
     .reduce((prevArr, currentVal) => {
-        return prevArr.concat(currentVal)
-    }, [])
+      return prevArr.concat(currentVal);
+    }, []);
 
-    console.log(transformedIngredients)
+  console.log(transformedIngredients);
 
-    if(transformedIngredients.length === 0){
-        transformedIngredients = <p>Please start adding Ingredients</p>
-    }
-    return (
-        <div className='Burger'>
-            <BurgerIngredient type="bread-top"/>
-                {transformedIngredients}
-            <BurgerIngredient type="bread-bottom"/>
-        </div>
-    );
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding Ingredients</p>;
+  }
+  return (
+    <div className="Burger">
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
 
-
-export default burger;
+export default withRouter(burger);
